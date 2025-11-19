928c8aa25d2e6c5419efd2bd0774f54689131aa8
luanqueiroz78 <luan.canuto1@aluno.ce.gov.br>
2025-11-19 13:25:44 -0300

    Projeto criado

---
+++ src/main/java/com/anm/estoque/Controller/EstoqueController.java
@@ -0,0 +1,26 @@
+package com.anm.estoque.controller;
+
+import com.anm.estoque.model.ProdutoModelo;
+import com.anm.estoque.service.EstoqueServico;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@RestController
+@CrossOrigin("*")
+public class EstoqueController {
+
+    @Autowired
+    private EstoqueServico es;
+
+    @GetMapping("/")
+    public void testeApi() {
+        System.out.println("A API está funcionando!");
+    }
+
+    @GetMapping("/listar")
+    public Iterable<ProdutoModelo> listar() {
+        return es.listar();
+    }
+}
